35cd76faa71f7f417f28ee750ddea0b098a433cc
VitPet <petrusenkovitaly@mail.ru>
2017-03-13 17:08:07 +0200

    change file

--- index.php
+++ index.php
@@ -1,33 +1,12 @@
 <?php
+   header("Access-Control-Allow-Origin: *");
+	$login = "cursor";
+	$password = "education";
 
-header("Access-Control-Allow-Origin: *");
-$method = $_SERVER['REQUEST_METHOD'];
-
-$c = true;
-if ( $method === 'POST' ) {
-
-	$project_name = trim($_POST["project_name"]);
-	$admin_email  = trim($_POST["admin_email"]);
-	$form_subject = trim($_POST["form_subject"]);
-
-	foreach ( $_POST as $key => $value ) {
-		if ( $value != "" && $key != "project_name" && $key != "admin_email" && $key != "form_subject" ) {
-			$message .= "
-			" . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
-				<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
-				<td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
-			</tr>
-			";
-		}
+	if ($login == $_POST['login'] && $password == $_POST['password']) {
+		echo("true");
 	}
-
-
-$message = "<table style='width: 100%;'>$message</table>";
-
-
-$headers = "MIME-Version: 1.0" . PHP_EOL .
-"Content-Type: text/html; charset=utf-8" . PHP_EOL .
-'From: '.adopt($project_name).' <'.petrusenkovitaly@mail.ru.'>' . PHP_EOL .
-'Reply-To: '.vitalii.petrusenko@gmail.com.'' . PHP_EOL;
-
-mail($admin_email, adopt($form_subject), $message, $headers );
+	else {
+		echo("false");
+	}
+?>
